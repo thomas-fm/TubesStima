1c351cb5c58fcaac37b3ce54694ab415f15601a0
Thomas Ferdinand M <56783537+thomas-fm@users.noreply.github.com>
2021-03-26 12:30:47 +0700

    added exception for invalid file type

--- NasiPadang/NasiPadang/Form1.Designer.cs
+++ NasiPadang/NasiPadang/Form1.Designer.cs
@@ -424,6 +424,7 @@ namespace NasiPadang
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "            ";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel2.ResumeLayout(false);
